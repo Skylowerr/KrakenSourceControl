42fc6a6fc02c6f4ed6012df69b3674b06a226de3
Skylowerr <gokceemirhan23@gmail.com>
2026-04-29 16:46:39 +0200

    Initial Commit

---
+++ KrakenSourceControl/KrakenSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrakenSourceControlApp.swift
+//  KrakenSourceControl
+//
+//  Created by Emirhan Gökçe on 29.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct KrakenSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
