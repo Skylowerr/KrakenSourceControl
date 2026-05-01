5a595e05fec9c832235abb4d39951e7ae80d812a
Skylowerr <gokceemirhan23@gmail.com>
2026-05-01 14:35:32 +0200

    Add HomeView & Modified

---
+++ KrakenSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  KrakenSourceControl
+//
+//  Created by Emirhan Gökçe on 1.05.2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
