519e7d8b2e82fdc62039ee97042650eb8968063e
Skylowerr <gokceemirhan23@gmail.com>
2026-05-01 14:28:08 +0200

    My second commit

--- KrakenSourceControl/ContentView.swift
+++ KrakenSourceControl/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("SKYLOWERR!")
+            
+            Button("Click Me"){
+                print("hello")
+                
+                
+                
+            }
         }
         .padding()
         .padding()
