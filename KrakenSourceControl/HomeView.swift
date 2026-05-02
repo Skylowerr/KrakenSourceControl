51513fd3b6bd914e1c7f4e2ec6374b88cb4a5866
Skylowerr <gokceemirhan23@gmail.com>
2026-05-02 12:22:48 +0200

    [Clean]  Delete analytics

--- KrakenSourceControl/HomeView.swift
+++ KrakenSourceControl/HomeView.swift
@@ -16,9 +16,6 @@ struct HomeView: View {
                 Text("Hi")
                 Text("Screen 2!")
             }
-            .onAppear{
-                //analytics
-            }
         }
     }
 }
