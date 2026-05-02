33d6a2205f2a1bb2002e9ee4f43eda04466d8349
Skylowerr <gokceemirhan23@gmail.com>
2026-05-02 12:32:25 +0200

    [Feature] Hello

--- KrakenSourceControl/HomeView.swift
+++ KrakenSourceControl/HomeView.swift
@@ -13,7 +13,7 @@ struct HomeView: View {
     var body: some View {
         ZStack{
             VStack{
-                Text("Hi")
+                Text("Hello")
                 Text("Screen 2!")
             }
         }
