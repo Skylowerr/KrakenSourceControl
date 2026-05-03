4568a57cfea97be353a39b009f0669b34101799e
Skylowerr <gokceemirhan23@gmail.com>
2026-05-03 12:19:19 +0200

    [Clean] Update docs

--- KrakenSourceControl/ContentView.swift
+++ KrakenSourceControl/ContentView.swift
@@ -18,6 +18,9 @@
  Rebase = Moving one branch on top of another branch
  Cherry picking = Duplicating (copying) one commit from one branch to another
  Pull Request (PR)= Request to merge branch
+ PR Merge = Merge all commits
+ PR Squash and Merge = Squash all commits into ONE and then merge ONE commit
+ 
  
  
  
