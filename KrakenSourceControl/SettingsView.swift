c7eff6e95e49c1ee193494f929e566a3e2d2e0b6
Skylowerr <gokceemirhan23@gmail.com>
2026-05-03 11:48:24 +0200

    [Feature] Add Settings View

---
+++ KrakenSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  KrakenSourceControl
+//
+//  Created by Emirhan Gökçe on 3.05.2026.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
