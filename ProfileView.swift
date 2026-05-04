8f5c7fedd362ff08b4ff91410539443a1cd405a7
Skylowerr <gokceemirhan23@gmail.com>
2026-05-04 10:31:26 +0200

    [Feature] Add Profile View

---
+++ ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  KrakenSourceControl
+//
+//  Created by Emirhan Gökçe on 4.05.2026.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Gokce")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
